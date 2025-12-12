49b139d77a8c16b4c22f61ba292b94cb58558a38
darioDTU <s194830@dtu.dk>
2025-12-12 16:42:46 +0000

    change db to daily

--- frontend/src/service/db_variables.ts
+++ frontend/src/service/db_variables.ts
@@ -1,12 +1,12 @@
 export const dbVariables: Record<string, string[]> = {
   'cmems_mod_glo_phy-thetao_anfc_0.083deg_P1D-m': ['thetao-Temperature'],
   'cmems_mod_glo_phy_my_0.083deg_P1D-m': ['thetao-Temperature', 'so-Salinity'],
-  'cmems_mod_glo_bgc_my_0.25deg_P1M-m': ['chl-Chlorophyll'],
+  'cmems_mod_glo_bgc_my_0.25deg_P1D-m': ['chl-Chlorophyll'],
 
 };
 
 export const dbYears: Record<string, string[]> = {
   'cmems_mod_glo_phy-thetao_anfc_0.083deg_P1D-m': ["2022", "2023", "2024", "2025"],
   'cmems_mod_glo_phy_my_0.083deg_P1D-m': Array.from({ length: 2021 - 2010 + 1 }, (_, i) => (2010 + i).toString()),
-  'cmems_mod_glo_bgc_my_0.25deg_P1M-m': Array.from({ length: 2025 - 2010 + 1 }, (_, i) => (2010 + i).toString()),
+  'cmems_mod_glo_bgc_my_0.25deg_P1D-m': Array.from({ length: 2025 - 2010 + 1 }, (_, i) => (2010 + i).toString()),
 };
